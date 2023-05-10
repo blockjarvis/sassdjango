3e4cbd432f63ff2f24ff3231c258b30ececa9764
Rafael Correa <tiobenedito@gmail.com>
2023-05-10 17:28:05 +0100

    vercel config deleted, env keys settled

--- goldproject/settings.py
+++ goldproject/settings.py
@@ -27,12 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p!_0j1$oc0!iax0%t35hrc5622ujp8%cwal()55!&y!*d($%3)'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','*']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- home/contexts.py
+++ home/contexts.py
@@ -13,7 +13,7 @@ def metal_prices(request):
              'XAG' : 'Silver',
              'XPT' : 'Platinum' }
     
-    access_key = 'fdbddc254601e7b9b4e7b65a81bbd5d1'
+    access_key = os.environ.get('API_KEY', '')
 
     resp = requests.get(
         'https://api.metalpriceapi.com/v1/latest'+'?api_key='+access_key+'&base='+base_currency+'&currencies='+symbol)
