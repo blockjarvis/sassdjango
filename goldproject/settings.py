92bdb3f86a75edba3884ee071b5749a8b10a3d46
Rafael Correa <tiobenedito@gmail.com>
2023-05-10 15:16:44 +0100

    key value settled

--- goldproject/settings.py
+++ goldproject/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = 'django-insecure-p!_0j1$oc0!iax0%t35hrc5622ujp8%cwal()55!&y!*d($%3)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
